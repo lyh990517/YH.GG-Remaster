bcee454ca6ac5b79bb3fabc06ade84fd2ba4bf44
lyh990517 <lyh990517@naver.com>
2023-03-21 15:52:23 +0900

    DI 설정

---
+++ app/src/main/java/yunho/compose/yhgg_remaster/data/api/ApiHelper.kt
@@ -0,0 +1,25 @@
+package yunho.compose.yhgg_remaster.data.api
+
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiHelper {
+    fun <T> create(
+        service: Class<T>,
+        baseUrl: String
+    ): T = Retrofit.Builder().apply {
+        baseUrl(baseUrl)
+        addConverterFactory(GsonConverterFactory.create(Gson()))
+        client(createOkHttpClient())
+    }.build().create(service)
+
+    private fun createOkHttpClient(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        return builder.build()
+    }
+}
