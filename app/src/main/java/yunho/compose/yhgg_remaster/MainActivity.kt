ded638b89f4d6de478f96a067bb39c110796a490
lyh990517 <lyh990517@naver.com>
2023-03-22 16:35:14 +0900

    액티비티 정리

--- app/src/main/java/yunho/compose/yhgg_remaster/MainActivity.kt
+++ app/src/main/java/yunho/compose/yhgg_remaster/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.lifecycleScope
@@ -26,47 +27,10 @@ import yunho.compose.yhgg_remaster.ui.theme.YHGGRemasterTheme
 class MainActivity : ComponentActivity() {
 
     val viewmodel: SummonerViewModel by viewModels()
-    val matchViewmodel: MatchViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         lifecycleScope.launch {
-            matchViewmodel.matchState.collect {
-                when (it) {
-                    is MatchState.Loading -> {
-                        Log.e("state", "Loading")
-                    }
-                    is MatchState.LoadIds -> {
-                        handleMatchIds(it)
-                    }
-                    is MatchState.Success -> {
-                        handleMatch(it)
-                    }
-                    is MatchState.Error -> {
-                        handleMatchError(it)
-                    }
-                    else -> {}
-                }
-            }
-        }
-        lifecycleScope.launch {
             viewmodel.getSummonerInfo("hide on bush")
-            viewmodel.summonerState.collect {
-                when (it) {
-                    is SummonerState.Loading -> {
-                        Log.e("state", "Loading")
-                    }
-                    is SummonerState.LoadLeagueEntry -> {
-                        handleLeagueEntry(it)
-                    }
-                    is SummonerState.Success -> {
-                        handleSuccess(it)
-                    }
-                    is SummonerState.Error -> {
-                        handleError(it)
-                    }
-                    else -> {}
-                }
-            }
         }
         setContent {
             YHGGRemasterTheme {
@@ -80,40 +44,12 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
-    private fun handleMatchError(it: MatchState.Error) {
-        Log.e("match", "error: ${it.e.message}")
-    }
-
-    private fun handleMatch(it: MatchState.Success) {
-        Log.e("match", "${it.matchData.info}")
-    }
-
-    private suspend fun handleMatchIds(it: MatchState.LoadIds) {
-        Log.e("Match", "${it.ids}")
-        for (id in it.ids) {
-            matchViewmodel.getMatchInfo(id)
-        }
-    }
-
-    private fun handleLeagueEntry(it: SummonerState.LoadLeagueEntry) {
-        Log.e("state", "${it.data}")
-    }
-
-    private fun handleError(it: SummonerState.Error) {
-        Log.e("state", "error: ${it.e.message}")
-    }
-
-    private suspend fun handleSuccess(it: SummonerState.Success) {
-        Log.e("state", "success: ${it.infoData}")
-        viewmodel.getSummonerLeague(it.infoData.id)
-        matchViewmodel.getMatchIds(it.infoData.puuid)
-    }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, viewModel: SummonerViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+    val state = viewModel.summonerState.collectAsState()
+    Text(text = "Hello ${state.value}!")
 }
 
 @Preview(showBackground = true)
