b33e10d2bf514ca6de9eb4e084e995168fb49d06
lyh990517 <lyh990517@naver.com>
2023-03-29 11:49:52 +0900

    디자인 업데이트

--- feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
+++ feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
@@ -4,9 +4,11 @@ import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -14,9 +16,11 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -96,7 +100,7 @@ fun DetailScreen(
                 summonerDTO = currentSummoner.value
             )
             MatchView(
-                Modifier.weight(3f),
+                Modifier.weight(6f),
                 itemList = matchList
             )
         }
@@ -112,25 +116,42 @@ fun SummonerView(
     leagueEntryDTO: List<LeagueEntryDTO>,
     summonerDTO: SummonerDTO
 ) {
-    Box(modifier = modifier.fillMaxSize()) {
+    BoxWithConstraints(modifier = modifier.fillMaxSize()) {
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "",
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = "backGround",
             contentScale = ContentScale.Crop,
-            modifier = modifier.fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         )
         Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
-            Row(modifier = Modifier) {
+            Row(modifier = Modifier.fillMaxWidth()) {
                 Image(
-                    painter = painterResource(id = R.drawable.teemo),
-                    contentDescription = "",
+                    painter = painterResource(id = R.drawable.profile_icon),
+                    contentDescription = "profile Image",
                     Modifier
+                        .padding(15.dp)
                         .width(100.dp)
                         .height(100.dp)
+                        .clip(RoundedCornerShape(10.dp)),
+                    contentScale = ContentScale.Crop
                 )
-                Text(text = "name", fontSize = 30.sp)
+                Text(
+                    text = "${summonerDTO.name}",
+                    fontSize = 30.sp,
+                    modifier = Modifier.padding(top = 15.dp),
+                    fontStyle = FontStyle.Normal,
+                    color = Color.White
+                )
             }
         }
+        Text(
+            text = "${summonerDTO.summonerLevel}",
+            modifier = Modifier
+                .padding(start = 50.dp, bottom = 10.dp)
+                .align(Alignment.BottomStart),
+            color = Color.White,
+            fontSize = 20.sp
+        )
     }
 }
 
@@ -169,7 +190,7 @@ fun TopImageViewPreview() {
     TopImageView(summonerDTO = dummySummonerDTO)
 }
 
-@Preview
+@Preview(heightDp = 300)
 @Composable
 fun SummonerViewPreview() {
     SummonerView(leagueEntryDTO = dummy, summonerDTO = dummySummonerDTO, modifier = Modifier)
