2c58dfc71c610fd51e58c9d85371f029df724cb7
lyh990517 <lyh990517@naver.com>
2023-03-29 14:17:34 +0900

    collapsing toolbar 적용

--- feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
+++ feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
@@ -2,9 +2,8 @@ package yunho.compose.summonerinfo
 
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -92,16 +92,28 @@ fun DetailScreen(
 
         }
     }
-    Scaffold() {
+    val scrollState = rememberScrollState(0)
+    Scaffold(
+        topBar = {
+            topScrollContent(
+                scrollState,
+                leagueEntryDTO = summonerLeague.value,
+                summonerDTO = currentSummoner.value
+            )
+        }
+    ) {
         Column {
             SummonerView(
-                Modifier.weight(1f),
+                Modifier
+                    .weight(1f)
+                    .sizeIn(minHeight = 200.dp, maxHeight = 400.dp),
                 leagueEntryDTO = summonerLeague.value,
                 summonerDTO = currentSummoner.value
             )
             MatchView(
                 Modifier.weight(6f),
-                itemList = matchList
+                itemList = matchList,
+                scrollState = scrollState
             )
         }
     }
@@ -111,54 +123,69 @@ fun DetailScreen(
 }
 
 @Composable
+fun topScrollContent(
+    scrollState: ScrollState,
+    leagueEntryDTO: List<LeagueEntryDTO>,
+    summonerDTO: SummonerDTO
+) {
+    val dynamicHeight = (250f - scrollState.value).coerceIn(130f, 250f)
+    TopAppBar(Modifier.heightIn(min = animateDpAsState(targetValue = dynamicHeight.dp).value).fillMaxWidth()) {
+        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.background),
+                contentDescription = "backGround",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.profile_icon),
+                        contentDescription = "profile Image",
+                        Modifier
+                            .padding(15.dp)
+                            .width(100.dp)
+                            .height(100.dp)
+                            .clip(RoundedCornerShape(10.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                    Text(
+                        text = "${summonerDTO.name}",
+                        fontSize = 30.sp,
+                        modifier = Modifier.padding(top = 15.dp),
+                        fontStyle = FontStyle.Normal,
+                        color = Color.White
+                    )
+                }
+            }
+            Text(
+                text = "${summonerDTO.summonerLevel}",
+                modifier = Modifier
+                    .padding(start = 50.dp, bottom = 10.dp)
+                    .align(Alignment.BottomStart),
+                color = Color.White,
+                fontSize = 20.sp
+            )
+        }
+    }
+}
+
+@Composable
 fun SummonerView(
     modifier: Modifier,
     leagueEntryDTO: List<LeagueEntryDTO>,
     summonerDTO: SummonerDTO
 ) {
-    BoxWithConstraints(modifier = modifier.fillMaxSize()) {
-        Image(
-            painter = painterResource(id = R.drawable.background),
-            contentDescription = "backGround",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-        Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Image(
-                    painter = painterResource(id = R.drawable.profile_icon),
-                    contentDescription = "profile Image",
-                    Modifier
-                        .padding(15.dp)
-                        .width(100.dp)
-                        .height(100.dp)
-                        .clip(RoundedCornerShape(10.dp)),
-                    contentScale = ContentScale.Crop
-                )
-                Text(
-                    text = "${summonerDTO.name}",
-                    fontSize = 30.sp,
-                    modifier = Modifier.padding(top = 15.dp),
-                    fontStyle = FontStyle.Normal,
-                    color = Color.White
-                )
-            }
-        }
-        Text(
-            text = "${summonerDTO.summonerLevel}",
-            modifier = Modifier
-                .padding(start = 50.dp, bottom = 10.dp)
-                .align(Alignment.BottomStart),
-            color = Color.White,
-            fontSize = 20.sp
-        )
-    }
 }
 
 @Composable
-private fun MatchView(modifier: Modifier = Modifier, itemList: MutableList<MatchState.Success>) {
-    LazyColumn(modifier = modifier) {
-        items(itemList) {
+private fun MatchView(
+    modifier: Modifier = Modifier,
+    itemList: MutableList<MatchState.Success>,
+    scrollState: ScrollState
+) {
+    Column(modifier = modifier.verticalScroll(scrollState)) {
+        itemList.forEach {
             MatchItem(it.matchData)
         }
     }
@@ -199,7 +226,7 @@ fun SummonerViewPreview() {
 @Preview
 @Composable
 fun MatchViewPreview() {
-    MatchView(itemList = mutableListOf())
+    MatchView(itemList = mutableListOf(), scrollState = ScrollState(0))
 }
 
 val dummy =
