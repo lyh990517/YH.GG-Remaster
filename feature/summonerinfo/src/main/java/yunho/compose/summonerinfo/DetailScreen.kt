ceb6bce709e47704b353eaef89880b87a5ce1b2a
lyh990517 <lyh990517@naver.com>
2023-03-29 15:22:44 +0900

    랭크뷰 적용

--- feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
+++ feature/summonerinfo/src/main/java/yunho/compose/summonerinfo/DetailScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
@@ -23,6 +24,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import yunho.compose.domain.model.LeagueEntryDTO
@@ -104,14 +106,12 @@ fun DetailScreen(
     ) {
         Column {
             SummonerView(
-                Modifier
-                    .weight(1f)
-                    .sizeIn(minHeight = 200.dp, maxHeight = 400.dp),
-                leagueEntryDTO = summonerLeague.value,
-                summonerDTO = currentSummoner.value
+                Modifier,
+                leagueEntry = summonerLeague.value,
+                scrollState = scrollState
             )
             MatchView(
-                Modifier.weight(6f),
+                Modifier.weight(1f),
                 itemList = matchList,
                 scrollState = scrollState
             )
@@ -129,7 +129,11 @@ fun topScrollContent(
     summonerDTO: SummonerDTO
 ) {
     val dynamicHeight = (250f - scrollState.value).coerceIn(130f, 250f)
-    TopAppBar(Modifier.heightIn(min = animateDpAsState(targetValue = dynamicHeight.dp).value).fillMaxWidth()) {
+    TopAppBar(
+        Modifier
+            .heightIn(min = animateDpAsState(targetValue = dynamicHeight.dp).value)
+            .fillMaxWidth()
+    ) {
         BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
             Image(
                 painter = painterResource(id = R.drawable.background),
@@ -173,9 +177,55 @@ fun topScrollContent(
 @Composable
 fun SummonerView(
     modifier: Modifier,
-    leagueEntryDTO: List<LeagueEntryDTO>,
-    summonerDTO: SummonerDTO
+    leagueEntry: List<LeagueEntryDTO>,
+    scrollState: ScrollState
 ) {
+    val dynamicHeight = (150f - scrollState.value).coerceIn(0f, 150f)
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .heightIn(
+                min = animateDpAsState(
+                    targetValue = dynamicHeight.dp
+                ).value
+            )
+    ) {
+        LazyColumn(
+            modifier.heightIn(
+                max = animateDpAsState(
+                    targetValue = dynamicHeight.dp
+                ).value
+            )
+        ) {
+            items(leagueEntry) {
+                RankItem(leagueEntry = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun RankItem(leagueEntry: LeagueEntryDTO) {
+    Text("League Id: ${leagueEntry.leagueId}")
+    Text("Summoner Id: ${leagueEntry.summonerId}")
+    Text("Summoner Name: ${leagueEntry.summonerName}")
+    Text("Queue Type: ${leagueEntry.queueType}")
+    Text("Tier: ${leagueEntry.tier}")
+    Text("Rank: ${leagueEntry.rank}")
+    Text("League Points: ${leagueEntry.leaguePoints}")
+    Text("Wins: ${leagueEntry.wins}")
+    Text("Losses: ${leagueEntry.losses}")
+    Text("Hot Streak: ${leagueEntry.hotStreak}")
+    Text("Veteran: ${leagueEntry.veteran}")
+    Text("Fresh Blood: ${leagueEntry.freshBlood}")
+    Text("Inactive: ${leagueEntry.inactive}")
+    leagueEntry.miniSeries?.let {
+        Text("Mini Series Wins: ${it.wins}")
+        Text("Mini Series Losses: ${it.losses}")
+        Text("Mini Series Target: ${it.target}")
+        Text("Mini Series Progress: ${it.progress}")
+    }
 }
 
 @Composable
@@ -220,7 +270,7 @@ fun TopImageViewPreview() {
 @Preview(heightDp = 300)
 @Composable
 fun SummonerViewPreview() {
-    SummonerView(leagueEntryDTO = dummy, summonerDTO = dummySummonerDTO, modifier = Modifier)
+    SummonerView(leagueEntry = dummy, modifier = Modifier, scrollState = ScrollState(0))
 }
 
 @Preview
